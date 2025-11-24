db4528e58f33a883c2b35dfe33ad6e34c7bb73f5
Dima <1224bbc@mail.ru>
2025-11-24 17:39:53 +0300

    add func get_date

--- src/widget.py
+++ src/widget.py
@@ -1,1 +1,28 @@
+from src.masks import get_mask_account, get_mask_card_number
 
+
+def mask_account_card(card: str) -> str:
+    """Функиция скрывающая название карты и ее номер и выдающая замаскированный номер"""
+    number_card = ""
+
+    for i in card:
+        if i.isdigit():
+            number_card += i
+    if len(number_card) == 16:
+        return get_mask_card_number(number_card)
+    else:
+        return get_mask_account(number_card)
+
+
+filter_card = mask_account_card("Maestro 7000792289606361")
+print(filter_card)
+
+import re
+def get_date(date: str) -> str:
+    """Функция возвращающая дату"""
+    date_filter = "".join(re.findall(r"\d{4}-\d{2}-\d{2}", date))
+    return f"{date_filter[-2:]}.{date_filter[5:7]}.{date_filter[:4]}"
+
+
+result = get_date("2024-03-11T02:26:18.671407")
+print(result)
