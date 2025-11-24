9bf22500fdce00202d35cdc3d577c7cb7ff8af20
Dima <1224bbc@mail.ru>
2025-11-24 22:11:51 +0300

    boss, i did it

--- src/widget.py
+++ src/widget.py
@@ -4,19 +4,20 @@ from src.masks import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(card: str) -> str:
-    """Функиция скрывающая название карты и ее номер и выдающая замаскированный номер"""
+    """Функиция скрывающая название карты и
+    ее номер и выдающая замаскированный номер"""
+    filter_type = re.split(r"[0-9]", card)
     number_card = ""
-
     for i in card:
         if i.isdigit():
             number_card += i
     if len(number_card) == 16:
-        return get_mask_card_number(number_card)
+        return "".join(filter_type) + get_mask_card_number(number_card)
     else:
-        return get_mask_account(number_card)
+        return "".join(filter_type) + get_mask_account(number_card)
 
 
-filter_card = mask_account_card("Счет 35383033474447895560")
+filter_card = mask_account_card("MasterCard 7158300734726758")
 print(filter_card)
 
 
