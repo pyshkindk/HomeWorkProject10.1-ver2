be86a08d1a063612e6cfbdb98cb1f7d57aa39829
Dima <1224bbc@mail.ru>
2025-12-16 10:38:53 +0300

    add docstrings

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,7 @@ import pytest
 
 @pytest.fixture
 def test_list():
+    """Изначальный список словарей используемый для тестов"""
     return [
         {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
         {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
