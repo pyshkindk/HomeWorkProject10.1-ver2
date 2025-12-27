9ed450b18fa1b6645a924dc51eb29234e59e8005
Dima <1224bbc@mail.ru>
2025-12-27 20:56:27 +0300

    add func transaction_descriptions

--- src/generators.py
+++ src/generators.py
@@ -1,4 +1,4 @@
-from typing import Any
+from typing import Generator, Any
 
 
 def filter_by_currency(transactions: list, currency: str) -> filter[Any]:
@@ -7,3 +7,9 @@ def filter_by_currency(transactions: list, currency: str) -> filter[Any]:
 
     filtred_curency = filter(lambda x: x["operationAmount"]["currency"]["code"] == currency, transactions)
     return filtred_curency
+
+
+def transaction_descriptions(transactions: list) -> Generator[str, None, None]:
+    """Генератор , который принимает список словарей с транзакциями
+    и возвращает описание каждой операции по очереди."""
+    yield from (x["description"] for x in transactions)
