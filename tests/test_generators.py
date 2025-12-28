28ee29ed531e1d350f407a56b6fb77d8e57feb13
Dima <1224bbc@mail.ru>
2025-12-28 09:29:16 +0300

    add tests for func transaction_descriptions

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -1,6 +1,6 @@
 import pytest
 
-from src.generators import filter_by_currency
+from src.generators import filter_by_currency, transaction_descriptions
 from tests.conftest import test_list_of_transactions
 
 
@@ -20,3 +20,13 @@ def test_filter_by_currency_empty():
     """Проверка на пустой список"""
     with pytest.raises(ValueError):
         filter_by_currency([], "")
+
+
+def test_transaction_descriptions(test_list_of_transactions):
+    """Проверка работы генератора вывода описания транзакций"""
+    result = transaction_descriptions(test_list_of_transactions)
+    assert next(result) == "Перевод организации"
+    assert next(result) == "Перевод со счета на счет"
+    assert next(result) == "Перевод со счета на счет"
+    assert next(result) == "Перевод с карты на карту"
+    assert next(result) == "Перевод организации"
